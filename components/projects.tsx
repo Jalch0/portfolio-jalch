6b67a13835585d8ab5235bccde57408aae842ae7
Jalch0 <jlabradorchacon@gmail.com>
2023-12-01 19:45:13 -0400

    Adding project.tsx, about.tsx, projects.tsx

---
+++ components/projects.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { projectsData } from "@/lib/data";
+import SectionHeading from "./section-heading";
+import Project from "./project";
+
+
+function Projects() {
+  return (
+    <section>
+      <SectionHeading>My projects</SectionHeading>
+      <div>
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
+
+
