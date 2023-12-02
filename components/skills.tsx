b4efbb55bfe8e92342f27772f0dfcc56c5e834c6
Jalch0 <jlabradorchacon@gmail.com>
2023-12-02 16:29:04 -0400

    Adding skills.tsx

---
+++ components/skills.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { skillsData } from "@/lib/data";
+import SectionHeading from "./section-heading";
+import { useSectionInView } from "@/lib/hook";
+import { motion } from "framer-motion";
+
+const fadeInAnimationsVariants = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  // This is delay for each tech/skills animations
+  animate: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.05 * index,
+    },
+  }),
+};
+
+function Skills() {
+  const { ref } = useSectionInView("Skills");
+
+  return (
+    <section
+      ref={ref}
+      className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
+    >
+      <SectionHeading>My Skills</SectionHeading>
+      <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
+        {skillsData.map((skill, index) => (
+          <motion.li
+            className="bg-white border border-black/[0.1] rounded-xl px-5 py-3"
+            key={index}
+            variants={fadeInAnimationsVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{
+              once: true,
+            }}
+            custom={index}
+          >
+            {skill}
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+export default Skills;
