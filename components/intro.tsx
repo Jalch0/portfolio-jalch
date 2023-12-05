0b4d44fed6a3953e010d1fbe22dcd11a91a7b914
Jalch0 <jlabradorchacon@gmail.com>
2023-12-05 15:11:29 -0400

    Finish intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,98 +3,107 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
 import Link from "next/link";
-import { BsArrowRight, BsLinkedin } from "react-icons/bs"
-import { HiDownload } from "react-icons/hi"
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hook";
 import { useActiveSectionContext } from "@/context/active-section-context";
 
 function Intro() {
+  const { ref } = useSectionInView("Home", 0.5);
+  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
 
-  const {ref} = useSectionInView('Home', 0.5);
-  const { setActiveSection, setTimeOfLastClick} = useActiveSectionContext();
-  
   return (
-    <section ref={ref} id="home" className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
-      <div className="flex items-center justify-center">
-        <div className="relative">
-          <motion.div
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              type: "tween",
-              duration: 0.2,
-            }}
-          >
-            <Image
-              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
-              alt="Javier Labrador"
-              width="192"
-              height="192"
-              quality="95"
-              priority={true}
-              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
-            />
-          </motion.div>
+    <section
+      ref={ref}
+      id="home"
+      className="mb-28 max-w-[60rem] text-center sm:mb-0 scroll-mt-[100rem]"
+    >
+      <div className="flex flex-col justify-between items-center md:flex-row" >
 
-          <motion.span
-            className="absolute bottom-0 right-0 text-4xl"
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              type: "spring",
-              stiffness: 125,
-              delay: 0.2,
-              duration: 0.7,
-            }}
-          >
-            👋
-          </motion.span>
+        {/* Intro text*/}
+
+        <motion.h1
+          className="text-white text-center w-100% mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl md:w-1/2 md:text-left"
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+        >
+          <span className="font-bold">Hello, I&apos;m Javier.</span> I&apos;m a{" "}
+          <span className="font-bold">Frontend developer</span> focused on   <span className="font-bold"> building beautiful</span> interfaces & experiences.
+        </motion.h1>
+
+        {/* Photo */}
+
+        <div className="flex mb-10 items-center md:pr-20">
+          <div className="relative">
+            <motion.div
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                type: "tween",
+                duration: 0.2,
+              }}
+            >
+              <Image
+                src="https://drive.google.com/uc?export=download&id=1FaotvuQ3M1qNyNIZHfS7Vmdul-Ymr2wf"
+                alt="Javier Labrador"
+                width="250"
+                height="250"
+                quality="95"
+                priority={true}
+                className="h-22 w-25 rounded-full object-cover border-[0.15rem] border-white shadow-xl"
+              />
+            </motion.div>
+
+            <motion.span
+              className="absolute bottom-2 right-4 text-4xl"
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                type: "spring",
+                stiffness: 125,
+                delay: 0.2,
+                duration: 0.7,
+              }}
+            >
+              ​🚀​
+            </motion.span>
+          </div>
         </div>
       </div>
 
-      <motion.h1
-        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+      {/* Buttons */}
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-4 px-4 text-lg font-medium"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
-      >
-        <span className="font-bold">Hello, I&apos;m Ricardo.</span> I&apos;m a{" "}
-        <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">8 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
-      </motion.h1>
-
-      <motion.div className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
-        initial={{opacity: 0, y: 100}}
-        animate={{ opacity: 1, y: 0}}
         transition={{
-            delay:0.1,
+          delay: 0.1,
         }}
-      
       >
-            <Link href="#contact" className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
-            onClick={() => {
-              setActiveSection("Contact");
-              setTimeOfLastClick(Date.now())
-            }}
-            >
-                Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
-            </Link>
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none border border-gray-900 focus:scale-110 hover:scale-110 hover:bg-gray-950 hover:border-white active:scale-105 transition"
+          onClick={() => {
+            setActiveSection("Contact");
+            setTimeOfLastClick(Date.now());
+          }}
+        >
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
 
-            <a className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack" href="/CV.pdf" download>
-                Download CV <HiDownload className="opacity-60 group-hover:translate-y-1 transition"/> 
-            </a>
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack"
+          href="/CV.pdf"
+          download
+        >
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
 
-            <a className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack" href="https://linkedin.com" target="_blank">
-                <BsLinkedin />
-            </a>
-
-            <a className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full text-[1.35rem] focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack" href="https://github.com" target="_blank">
-                <FaGithubSquare />
-            </a>
       </motion.div>
-
     </section>
   );
 }
