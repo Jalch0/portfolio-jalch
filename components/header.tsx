4f2ab999b66bfde99411153f7e768b95de41665e
Jalch0 <jlabradorchacon@gmail.com>
2023-12-05 13:33:34 -0400

    Change design in header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -14,7 +14,7 @@ function Header() {
   return (
     <header className="z-[999] relative">
       <motion.div
-        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-90 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
+        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-gray border-opacity-40 bg-gray-900 bg-opacity-90 shadow-lg shadow-black/[0.03] backdrop-blur-[0.4rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
