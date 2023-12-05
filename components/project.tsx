f7c09ed64dfd06eafdee9e2ac2a955af17d26e11
Jalch0 <jlabradorchacon@gmail.com>
2023-12-05 18:24:08 -0400

    Finish project.tsx

--- components/project.tsx
+++ components/project.tsx
@@ -5,6 +5,7 @@ import { projectsData } from "@/lib/data";
 import Image from "next/image";
 import { useScroll, useTransform } from "framer-motion";
 import { motion } from "framer-motion";
+import { MdArrowOutward } from "react-icons/md";
 
 type ProjectProps = (typeof projectsData)[number];
 
@@ -13,53 +14,96 @@ export default function Project({
   description,
   tags,
   imageUrl,
+  url,
 }: ProjectProps) {
   const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ["0 1", "1.33 1"],
   });
-  const scaleProgress = useTransform(scrollYProgress, [0,1], [0.8, 1]);
-  const opacityProgress = useTransform(scrollYProgress, [0,1], [0.6, 1]);
-  
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.9, 1]);
+  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.2, 1]);
 
   return (
-    <motion.div ref={ref} style={{ scale: scaleProgress, opacity: opacityProgress }} className="group mb-3 sm:mb-8 last:mb-0 ">
-      <section
-        className=" bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition group-even:pl-8"
-      >
-        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem] ">
-          <h3 className="text-2xl font-semibold">{title}</h3>
-          <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
-          <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
-            {tags.map((tag, index) => (
-              <li
-                className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full"
-                key={index}
-              >
-                {tag}
-              </li>
-            ))}
-          </ul>
-        </div>
+    <motion.div
+      ref={ref}
+      style={{ scale: scaleProgress, opacity: opacityProgress }}
+      className="group mb-3 sm:mb-8 last:mb-0"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+    >
+      <a href={url} target="_blank">
+        <section className=" bg-gray-900 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 sm:h-[20rem] hover:bg-opacity-50 transition group-even:pl-8 flex justify-between">
+          <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:order-2 ">
+            <h3 className="text-2xl font-semibold text-white flex items-center group-hover:text-sky-700">
+              {title}
+              <MdArrowOutward className="opacity-70 group-hover:-translate-y-1 group-hover:translate-x-1 group-hover:text-sky-700 transition" />{" "}
+            </h3>
+            <p className="mt-2 leading-relaxed text-gray-300">{description}</p>
+            <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+              {tags.map((tag, index) => (
+                <li
+                  className="bg-gray-800 px-3 py-1 text-[0.7rem] uppercase tracking-wider font-bold text-sky-700 rounded-full"
+                  key={index}
+                >
+                  {tag}
+                </li>
+              ))}
+            </ul>
+          </div>
 
-        <Image
-          src={imageUrl}
-          quality={95}
-          alt="Project I worked on"
-          className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition group-hover:scale-[1.04] 
-        group-hover:-transition-x-3 
-        group-hover:translate-y-3 
-        group-hover:-rotate-2 
-
-        group-even:group-hover:transition-x-3 
-        group-even:group-hover:translate-y-3 
-        group-even:group-hover:rotate-2 
-
-        group-even:-right-[initial] 
-        group-even:-left-40"
-        />
-      </section>
+          <div className="flex items-center justify-center">
+            <Image
+              src={imageUrl}
+              height="250"
+              width="250"
+              quality="95"
+              alt="Project I worked on"
+              className="h-auto w-full object-cover transition shadow-2xl group-hover:scale-[1.04]"
+            />
+          </div>
+        </section>
+      </a>
     </motion.div>
   );
+}
+
+{
+  /* <motion.div ref={ref} style={{ scale: scaleProgress, opacity: opacityProgress }} className="group mb-3 sm:mb-8 last:mb-0 ">
+<section
+  className=" bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition group-even:pl-8"
+>
+  <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem] ">
+    <h3 className="text-2xl font-semibold">{title}</h3>
+    <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
+    <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+      {tags.map((tag, index) => (
+        <li
+          className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full"
+          key={index}
+        >
+          {tag}
+        </li>
+      ))}
+    </ul>
+  </div>
+
+  <Image
+    src={imageUrl}
+    quality={95}
+    alt="Project I worked on"
+    className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition group-hover:scale-[1.04] 
+  group-hover:-transition-x-3 
+  group-hover:translate-y-3 
+  group-hover:-rotate-2 
+
+  group-even:group-hover:transition-x-3 
+  group-even:group-hover:translate-y-3 
+  group-even:group-hover:rotate-2 
+
+  group-even:-right-[initial] 
+  group-even:-left-40"
+  />
+</section>
+</motion.div> */
 }
--- lib/data.ts
+++ lib/data.ts
@@ -66,7 +66,8 @@ export const projectsData = [
     description:
       "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
     tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+    imageUrl: "https://drive.google.com/uc?export=download&id=1mwW7WWvJS1sOe2wwLrl8VhwH26BukUBr",
+    url: "https://google.com"
   },
   {
     title: "rmtDev",
@@ -74,6 +75,8 @@ export const projectsData = [
       "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
     tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
     imageUrl: rmtdevImg,
+    url: "https://google.com"
+
   },
   {
     title: "Word Analytics",
@@ -81,6 +84,8 @@ export const projectsData = [
       "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
     tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
     imageUrl: wordanalyticsImg,
+    url: "https://google.com"
+
   },
 ] as const;
 
